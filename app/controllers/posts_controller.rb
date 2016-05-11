ba4ad304c2c49f80d6928b5da04a52062e586e06
Keith Bachand <keithbachand14@gmail.com>
2016-05-11 10:30:23 -0400

    created news feed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:update, :destroy, :edit]
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      @post.create_activity key: 'post.created', owner: @post.user
+      respond_to do |format|
+        format.html {redirect_to user_path(@post.user.username), notice: "Post Created"}
+      end
+    else 
+      redirect_to user_path(@post.user.username), notice: "Something went wrong."
+    end
+  end
+
+  def edit
+    
+  end
+
+  def update
+    if @post.update(post_params)
+      respond_to do |format|
+        format.html {redirect_to user_path(@post.user.username), notice: "Post updated."}
+      end
+    else 
+      redirect_to user_path(@post.user.username), notice: "Something went wrong."
+    end
+  end
+
+  def destroy
+    @post.destroy
+    respond_to do |format|
+      format.html {redirect_to user_path(@post.user.username), notice: "Post was deleted!"}
+  end
+end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end 
+  
+end 
---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+include PublicActivity::Model
+  belongs_to :user
+  validates :user_id, :content, presence: true
+
+end
